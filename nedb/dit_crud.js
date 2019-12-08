0e4d61c5f7e77e7db002a20b9d8fb615b9eb2a05
weiboyang <weiboyang@movtile.com>
2019-12-09 01:31:32 +0800

    Add: Project-Episode-Scene-Shot-Take-Construct-Destruct

--- nedb/dit_crud.js
+++ nedb/dit_crud.js
@@ -1,4 +1,5 @@
 /*globals __dirname */
+const { isValidArray } = require('../utils/utils.js');
 const Nedb = require('nedb');
 const path = require('path');
 
@@ -262,6 +263,299 @@ function updateScene(userid, scene) {
     });
 }
 
+function getShot(userid, projectid, episodeid, sceneid, id) {
+    return new Promise(function(resolve, reject) {
+        db.findOne(
+            { userid, mag: 'shot', projectid, episodeid, sceneid, id },
+            (err, doc) => {
+                if (err) {
+                    reject({
+                        state: 1,
+                        message: 'DB Error',
+                        data: {
+                            detail: 'FindOne 数据出错',
+                            error: err
+                        }
+                    });
+                }
+                resolve({
+                    state: 0,
+                    data: doc
+                });
+            }
+        );
+    });
+}
+
+function addShot(userid, shot) {
+    return new Promise(function(resolve, reject) {
+        db.insert({ userid, mag: 'shot', ...shot, alive: true }, (err, doc) => {
+            if (err) {
+                reject({
+                    state: 1,
+                    message: 'DB Error',
+                    data: {
+                        detail: 'Insert 数据出错',
+                        error: err
+                    }
+                });
+            }
+            resolve({
+                state: 0,
+                data: doc
+            });
+        });
+    });
+}
+function updateShot(userid, shot) {
+    return new Promise(function(resolve, reject) {
+        db.update(
+            {
+                userid,
+                mag: 'shot',
+                projectid: shot.projectid,
+                episodeid: shot.episodeid,
+                sceneid: shot.sceneid,
+                id: shot.id
+            },
+            { $set: shot },
+            (err, amt) => {
+                if (err) {
+                    reject({
+                        state: 1,
+                        message: 'DB Error',
+                        data: {
+                            detail: 'Update 数据出错',
+                            error: err
+                        }
+                    });
+                }
+                resolve({
+                    state: 0,
+                    data: {
+                        amt
+                    }
+                });
+            }
+        );
+    });
+}
+
+function getTake(userid, projectid, episodeid, sceneid, shotid, id) {
+    return new Promise(function(resolve, reject) {
+        db.findOne(
+            { userid, mag: 'take', projectid, episodeid, sceneid, shotid, id },
+            (err, doc) => {
+                if (err) {
+                    reject({
+                        state: 1,
+                        message: 'DB Error',
+                        data: {
+                            detail: 'FindOne 数据出错',
+                            error: err
+                        }
+                    });
+                }
+                resolve({
+                    state: 0,
+                    data: doc
+                });
+            }
+        );
+    });
+}
+function addTake(userid, take) {
+    return new Promise(function(resolve, reject) {
+        db.insert({ userid, mag: 'take', ...take, alive: true }, (err, doc) => {
+            if (err) {
+                reject({
+                    state: 1,
+                    message: 'DB Error',
+                    data: {
+                        detail: 'Insert 数据出错',
+                        error: err
+                    }
+                });
+            }
+            resolve({
+                state: 0,
+                data: doc
+            });
+        });
+    });
+}
+function updateTake(userid, take) {
+    return new Promise(function(resolve, reject) {
+        db.update(
+            {
+                userid,
+                mag: 'take',
+                projectid: take.projectid,
+                episodeid: take.episodeid,
+                sceneid: take.sceneid,
+                shotid: take.shotid,
+                id: take.id
+            },
+            { $set: take },
+            (err, amt) => {
+                if (err) {
+                    reject({
+                        state: 1,
+                        message: 'DB Error',
+                        data: {
+                            detail: 'Update 数据出错',
+                            error: err
+                        }
+                    });
+                }
+                resolve({
+                    state: 0,
+                    data: {
+                        amt
+                    }
+                });
+            }
+        );
+    });
+}
+function exclude(list, sublist) {
+    return list.reduce((pre, item) => {
+        if (!sublist.includes(item)) {
+            pre.push(item);
+        }
+        return pre;
+    }, []);
+}
+
+/**
+ *
+ * @param {Object} listObj: { projectList, episodelist, scenelist, shotList, takeList }
+ */
+function construct(listObj) {
+    let { projectList, episodeList, sceneList, shotList, takeList } = listObj;
+    // console.log('takeList:\n', JSON.stringify(takeList));
+    shotList.forEach(shot => {
+        let { userid, projectid, episodeid, sceneid } = shot;
+        let shotid = shot.id;
+        shot.children = takeList.filter(take => {
+            return (
+                userid === take.userid &&
+                projectid === take.projectid &&
+                episodeid === take.episodeid &&
+                sceneid === take.sceneid &&
+                shotid === take.shotid
+            );
+        });
+        takeList = exclude(takeList, shot.children);
+    });
+
+    // console.log('takeList:\n', JSON.stringify(takeList));
+    // console.log('shotList:\n', JSON.stringify(shotList));
+    sceneList.forEach(scene => {
+        let { userid, projectid, episodeid } = scene;
+        let sceneid = scene.id;
+        scene.children = shotList.filter(shot => {
+            return (
+                userid === shot.userid &&
+                projectid === shot.projectid &&
+                episodeid === shot.episodeid &&
+                sceneid === shot.sceneid
+            );
+        });
+        shotList = exclude(shotList, scene.children);
+    });
+    // console.log('sceneList:\n', JSON.stringify(sceneList));
+    episodeList.forEach(episode => {
+        let { userid, projectid } = episode;
+        let episodeid = episode.id;
+        episode.children = sceneList.filter(scene => {
+            return (
+                userid === scene.userid &&
+                projectid === scene.projectid &&
+                episodeid === scene.episodeid
+            );
+        });
+        sceneList = exclude(sceneList, episode.children);
+    });
+    // console.log('episodeList:\n', JSON.stringify(episodeList));
+
+    if (isValidArray(projectList)) {
+        projectList.forEach(project => {
+            let userid = project.userid;
+            let projectid = project.id;
+            project.children = episodeList.filter(episode => {
+                return (
+                    userid === episode.userid && projectid === episode.projectid
+                );
+            });
+            episodeList = exclude(episodeList, project.children);
+        });
+        return projectList;
+    } else {
+        let projectObj = {};
+        //TODO: 保证都是同一个用户
+        episodeList.forEach(episode => {
+            let children = projectObj[episode.projectid];
+            if (isValidArray(children)) {
+                children.push(episode);
+            } else {
+                projectObj[episode.projectid] = [episode];
+            }
+        });
+        // console.log('projectObj\n', projectObj);
+        let keys = Object.keys(projectObj);
+        projectList = [];
+        keys.forEach(key => {
+            projectList.push({
+                id: key,
+                children: projectObj[key]
+            });
+        });
+    }
+    return projectList;
+}
+
+function destruct(project) {
+    let rawEpisodeList = project.children;
+    let episodeList = [];
+    let sceneList = [];
+    let shotList = [];
+    let takeList = [];
+    let swapScenList = [];
+    let swapShotList = [];
+    episodeList = rawEpisodeList.reduce((pre, cur) => {
+        if (isValidArray(cur.children)) {
+            swapScenList = swapScenList.concat(cur.children);
+            // swapScenList = [...swapScenList, ...cur.children];
+            delete cur['children'];
+        }
+        pre.push(cur);
+        return pre;
+    }, []);
+    sceneList = swapScenList.reduce((pre, cur) => {
+        if (isValidArray(cur.children)) {
+            swapShotList = swapShotList.concat(cur.children);
+            delete cur['children'];
+        }
+        pre.push(cur);
+        return pre;
+    }, []);
+    shotList = swapShotList.reduce((pre, cur) => {
+        if (isValidArray(cur.children)) {
+            takeList = takeList.concat(cur.children);
+            delete cur['children'];
+        }
+        pre.push(cur);
+        return pre;
+    }, []);
+    return {
+        episodeList,
+        sceneList,
+        shotList,
+        takeList
+    };
+}
+
 module.exports = {
     db,
     clearDB,
@@ -274,5 +568,13 @@ module.exports = {
     updateEpisode,
     getScene,
     addScene,
-    updateScene
+    updateScene,
+    getShot,
+    addShot,
+    updateShot,
+    getTake,
+    addTake,
+    updateTake,
+    destruct,
+    construct
 };
--- test/test.js
+++ test/test.js
@@ -13,17 +13,32 @@ const {
     updateEpisode,
     getScene,
     addScene,
-    updateScene
+    updateScene,
+    getShot,
+    addShot,
+    updateShot,
+    getTake,
+    addTake,
+    updateTake,
+    construct,
+    destruct
 } = require('../nedb/dit_crud');
-console.log('before');
-const { isNull, isValidPlainObject } = require('../utils/utils');
+
+const { isNull, isValidPlainObject, isValidArray } = require('../utils/utils');
 
 describe('DIT', function() {
     describe('DB API', function() {
         initDB();
         let userid = 'tester';
         let projectid = 'Project-00001';
-
+        let project = {
+            userid,
+            id: projectid,
+            name: '大江大河'
+        };
+        let updatedProject = Object.assign({}, project, {
+            name: '大江大海一箱啤酒'
+        });
         describe('clearDB', function() {
             it('clearing: ', function() {
                 return clearDB().then(v => {
@@ -40,10 +55,7 @@ describe('DIT', function() {
                 });
             });
             it('addProject', function() {
-                return addProject(userid, {
-                    id: projectid,
-                    name: '大江大河'
-                }).then(v => {
+                return addProject(userid, project).then(v => {
                     // console.log('v: ', v);
                     assert(
                         isValidPlainObject(v.data),
@@ -52,10 +64,7 @@ describe('DIT', function() {
                 });
             });
             it('updateProject', function() {
-                return updateProject(userid, {
-                    id: projectid,
-                    name: '大江大海一箱啤酒'
-                }).then(v => {
+                return updateProject(userid, updatedProject).then(v => {
                     // console.log('v: ',v);
                     assert(v.data.amt === 1, 'should return 1 if updated');
                 });
@@ -74,7 +83,8 @@ describe('DIT', function() {
         let episode = {
             id: episodeid,
             name: '大学教室',
-            projectid
+            projectid,
+            userid
         };
         let updatedEpisode = Object.assign({}, episode, { name: '大学操场' });
 
@@ -105,6 +115,7 @@ describe('DIT', function() {
         let scene = {
             id: sceneid,
             name: '大学教室',
+            userid,
             projectid,
             episodeid
         };
@@ -137,6 +148,145 @@ describe('DIT', function() {
                         );
                     }
                 );
+            });
+        });
+
+        let shotid = 'Shot-0001';
+        let shot = {
+            id: shotid,
+            userid,
+            projectid,
+            episodeid,
+            sceneid,
+            name: 'Shot-0001'
+        };
+        let updatedShot = Object.assign({}, shot, {
+            name: 'updated Shot-0001'
+        });
+        describe('Shot', function() {
+            it('getShot 1', function() {
+                return getShot(
+                    userid,
+                    projectid,
+                    episodeid,
+                    sceneid,
+                    shotid
+                ).then(v => {
+                    assert(isNull(v.data), 'should return null');
+                });
+            });
+            it('AddShot', function() {
+                return addShot(userid, shot).then(v => {
+                    assert(isValidPlainObject(v.data), 'should be an object');
+                });
+            });
+            it('updateShot', function() {
+                return updateShot(userid, updatedShot).then(v => {
+                    assert(v.data.amt == 1, 'should be 1');
+                });
+            });
+            it('getShot 2', function() {
+                return getShot(
+                    userid,
+                    projectid,
+                    episodeid,
+                    sceneid,
+                    shotid
+                ).then(v => {
+                    assert(isValidPlainObject(v.data), 'should be an object');
+                });
+            });
+        });
+
+        let takeid = 'Take-0001';
+        let take = {
+            id: takeid,
+            userid,
+            projectid,
+            episodeid,
+            sceneid,
+            shotid,
+            name: 'Take-0001'
+        };
+        let updatedTake = Object.assign({}, take, {
+            name: 'updated Take-0001'
+        });
+        describe('Take', function() {
+            it('getTake 1', function() {
+                return getTake(
+                    userid,
+                    projectid,
+                    episodeid,
+                    sceneid,
+                    shotid,
+                    takeid
+                ).then(v => {
+                    assert(isNull(v.data), 'should return null');
+                });
+            });
+            it('AddTake', function() {
+                return addTake(userid, take).then(v => {
+                    assert(isValidPlainObject(v.data), 'should be an object');
+                });
+            });
+            it('updateTake', function() {
+                return updateTake(userid, updatedTake).then(v => {
+                    assert(v.data.amt == 1, 'should be 1');
+                });
+            });
+
+            it('getTake 2', function() {
+                return getTake(
+                    userid,
+                    projectid,
+                    episodeid,
+                    sceneid,
+                    shotid,
+                    takeid
+                ).then(v => {
+                    assert(isValidPlainObject(v.data), 'should be an object');
+                });
+            });
+        });
+        let listObj = null;
+        describe('Destruct', function() {
+            let childrenTake = [take];
+            // console.log('childrenTake:\n', childrenTake);
+            let childrenShot = [
+                Object.assign({}, shot, { children: childrenTake })
+            ];
+            // console.log('childrenShot:\n', childrenShot);
+            let childrenScene = [
+                Object.assign({}, scene, { children: childrenShot })
+            ];
+            // console.log('childrenScene:\n', childrenScene);
+            let childrenEpisode = [
+                Object.assign({}, episode, { children: childrenScene })
+            ];
+            // console.log('childrenEpisod:\n', childrenEpisode);
+            let complex = Object.assign({}, project, {
+                children: childrenEpisode
+            });
+            // console.log('complex:\n', complex);
+            it('simple', function() {
+                listObj = destruct(complex);
+                // console.log(listObj);
+                assert(
+                    listObj &&
+                        Object.keys(listObj).length === 4 &&
+                        isValidArray(listObj.episodeList) &&
+                        isValidArray(listObj.sceneList) &&
+                        isValidArray(listObj.shotList) &&
+                        isValidArray(listObj.takeList)
+                );
+            });
+        });
+        describe('Construct', function() {
+            it('simple', function() {
+                let ret = construct(listObj);
+                // console.log('ret\n', ret);
+                // console.log('ret\n', JSON.stringify(ret));
+                assert(isValidArray(ret), 'Should be an array');
             });
         });
     });
